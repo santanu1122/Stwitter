6bf123934b2e41627a1f8bbd575bba1ea7e3a434
Andrew Long <aslong87@gmail.com>
2012-05-21 19:42:41 -0700

    Add some style and images
    Polish the ui up a bit with Launch image, icon, status bar,
    and nav bar tint.

--- Stwitter/Stwitter/AppDelegate.m
+++ Stwitter/Stwitter/AppDelegate.m
@@ -15,6 +15,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleBlackTranslucent];
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationNone];
+    UIColor *tintColor = [UIColor colorWithRed:18/255 green:44.0/255 blue:63.0/255 alpha:1.0];
+    [[UINavigationBar appearance] setTintColor:tintColor];
+    [[UISearchBar appearance] setTintColor:tintColor];
     return YES;
 }
 							
@@ -22,6 +27,7 @@
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
     // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationNone];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -38,6 +44,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationNone];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
